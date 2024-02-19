5440f1943c5a572de3e277f5d9e3dd7b1cc570ad
sbx_user1051 <sbx_user1051@169.254.49.157>
2024-02-19 10:38:53 +0000

    Mon Feb 19 10:38:53 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -5,18 +5,24 @@ object CheckoutSolution {
         'A' to 50,
         'B' to 30,
         'C' to 20,
-        'D' to 15
+        'D' to 15,
+        'E' to 40
     )
 
     private val offers = mapOf(
-        'A' to Pair(3, 130),
-        'B' to Pair(2, 45)
+        'A' to listOf(Pair(3, 130), Pair(5, 200)),
+        'B' to listOf(Pair(2, 45)),
+        'E' to listOf(Pair(2, 80))
+    )
+
+    private val bonusOffers = mapOf(
+        'E' to Pair('B', 1)
     )
 
     fun checkout(skus: String): Int {
         if (!skus.all { it in prices.keys }) return -1
 
-        val itemCounts = skus.groupingBy { it } .eachCount()
+        val itemCounts = skus.groupingBy { it } .eachCount().toMutableMap()
 
         val totalPrice = itemCounts.entries.sumOf {(item, count) ->
             val (offerQuantity, offerPrice) = offers[item] ?: Pair(0, 0)
@@ -32,4 +38,6 @@ object CheckoutSolution {
 
         return totalPrice
     }
+
+    private fun calculatePriceForItem(item: Char, count: Int): Int
 }
