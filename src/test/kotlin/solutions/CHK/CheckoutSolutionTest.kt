c4c87637588e47ae45a50b1ff14873149b076ba7
sbx_user1051 <sbx_user1051@169.254.49.157>
2024-02-19 10:20:41 +0000

    Mon Feb 19 10:20:41 UTC 2024

--- src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -3,10 +3,23 @@ package solutions.CHK
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import solutions.CHK.CheckoutSolution.checkout
 
 class CheckoutSolutionTest {
 
     @Test
-    fun checkout() {
+    fun testSingleItem() {
+        assertEquals(50, checkout("A"))
     }
+
+    @Test
+    fun testMultipleItems(){
+        assertEquals(80, checkout("AB"))
+    }
+
+    @Test
+    fun testSpecialOffer() {
+        assertEquals(130, checkout("AAA"))
+    }
+
 }
