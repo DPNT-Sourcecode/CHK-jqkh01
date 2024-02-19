35b733a170903486331f333287cd5f76c6cc6824
sbx_user1051 <sbx_user1051@169.254.49.157>
2024-02-19 10:06:31 +0000

    Mon Feb 19 10:06:31 UTC 2024

---
+++ src/test/kotlin/solutions/HLO/HelloSolutionTest.kt
@@ -0,0 +1,14 @@
+package solutions.HLO
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import solutions.HLO.HelloSolution.hello
+
+class HelloSolutionTest {
+
+    @Test
+    fun testHello() {
+        assertEquals("Hello world", hello("Hoang"))
+    }
+}
