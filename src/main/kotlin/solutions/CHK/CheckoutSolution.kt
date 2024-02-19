8806a79ee66527120fae805da06d7ab5ef548208
sbx_user1051 <sbx_user1051@169.254.49.157>
2024-02-19 10:29:41 +0000

    Mon Feb 19 10:29:41 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -1,9 +1,21 @@
 package solutions.CHK
 
 object CheckoutSolution {
-    private val prices = mapOf
+    private val prices = mapOf(
+        'A' to 50,
+        'B' to 30,
+        'C' to 20,
+        'D' to 15
+    )
+
+    private val offers = mapOf(
+        'A' to Pair(3, 130),
+        'B' to Pair(2, 45)
+    )
 
     fun checkout(skus: String): Int {
-        if (!items.all)
+        if (!skus.all { it in prices.keys }) return -1
+
+        val itemCounts
     }
 }
